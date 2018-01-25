0dd43cff7c52e28277334cea1e633561ad19311d
Malcolm Jones <malcolm@adobe.com>
2018-01-24 21:44:51 -0500

    chg: fixed main.go

--- config/config.go
+++ config/config.go
@@ -9,12 +9,11 @@ import (
 	"strconv"
 	"strings"
 
-	log "github.com/behance/go-logging/log"
+	logUtils "github.com/bossjones/go-chatbot-lab/shared/log-utils"
 )
 
 // Config -
 type Config struct {
-	// ********************************************
 	ConfigFile string `json:"configFile"`
 	// "name": "chatbot",
 	Name string `json:"name"`
@@ -50,8 +49,11 @@ type Config struct {
 	IsContainer bool `json:"isContainer"`
 	// "ssl-cert-location": "/etc/chatbot",
 	SSLCertLocation string `json:"ssl-cert-location"`
-	// ********************************************
+	// "log-level": "debugging level"
+	Debug bool `json:"logLevel"`
 }
+
+var chatLog = logUtils.NewLogger()
 
 // NewConfig -
 func NewConfig(file string) (Config, error) {
@@ -85,6 +87,7 @@ func (c *Config) LoadFromEnv() {
 	setValueFromEnv(&c.KVttl, "CHATBOT_KV_TTL")
 	setBoolValueFromEnv(&c.IsContainer, "CHATBOT_IS_CONTAINER")
 	setValueFromEnv(&c.SSLCertLocation, "CHATBOT_SSL_CERT_LOCATION")
+	setBoolValueFromEnv(&c.Debug, "CHATBOT_DEBUG")
 }
 
 // LoadFromConfigFile -
@@ -93,12 +96,12 @@ func (c *Config) LoadFromConfigFile(configFile string) error {
 	c.ConfigFile = configFile
 	jsonSrc, err := ioutil.ReadFile(c.ConfigFile)
 	if err != nil {
-		log.Warn("Error reading config.", "error", err)
+		chatLog.Warn("Error reading config.", "error", err)
 		return err
 	}
 	err = json.Unmarshal(jsonSrc, &c)
 	if err != nil {
-		log.Warn("Error parsing config.", "error", err)
+		chatLog.Warn("Error parsing config.", "error", err)
 
 		return err
 	}
@@ -149,7 +152,7 @@ func setSliceValueFromEnv(field *[]string, envVar string) {
 		apps := make([]string, 0)
 		err := json.Unmarshal([]byte(env), &apps)
 		if err != nil {
-			log.Error("Error parsing slice in config.", "variable", envVar, "value", env)
+			chatLog.Error("Error parsing slice in config.", "variable", envVar, "value", env)
 		}
 		*field = apps
 	}
@@ -161,9 +164,76 @@ func setSliceValueFromEnv(field *[]string, envVar string) {
 // 		var err error
 // 		*field, err = strconv.Atoi(env)
 // 		if err != nil {
-// 			log.Error("Invalid environment variable", "var", envVar, "value", env)
+// 			chatLog.Error("Invalid environment variable", "var", envVar, "value", env)
 // 		}
 // 	}
+// }
+
+// //Validate checks config values for obvious problems.
+// func (c *Config) Validate() error {
+// 	var e error
+// 	if _, err := strconv.Atoi(c.APIServerPort); err != nil {
+// 		e = errors.New(APIServerPortErr + c.APIServerPort)
+// 	}
+// 	if !strings.Contains("sqlite3,mysql", c.DBEngine) {
+// 		e = errors.New(DBEngineErr + c.DBEngine)
+// 	}
+// 	if c.DBPath == "" {
+// 		e = errors.New(DBPathErr + c.DBPath)
+// 	}
+// 	//mysql needs usernmae password and database
+// 	if c.DBEngine != "sqlite3" && c.DBUsername == "" {
+// 		e = errors.New(DBUsernameErr + c.DBUsername)
+// 	}
+// 	if c.DBEngine != "sqlite3" && c.DBPassword == "" {
+// 		e = errors.New(DBPasswordErr + c.DBPassword)
+// 	}
+// 	if c.DBEngine != "sqlite3" && c.DBDatabase == "" {
+// 		e = errors.New(DBDatabaseErr + c.DBDatabase)
+// 	}
+// 	if c.MarathonMaster == "" {
+// 		e = errors.New(MarathonMasterErr + c.MarathonMaster)
+// 	}
+// 	if c.MesosMaster == "" {
+// 		e = errors.New(MesosMasterErr + c.MesosMaster)
+// 	}
+// 	if c.MesosAuthenticationPrincipal != "" && c.MesosAuthenticationSecretFile == "" {
+// 		e = errors.New("Cannot have empty Mesos authentication file if principal is specified")
+// 	}
+// 	if c.MarathonAuthenticationPrincipal != "" && c.MarathonAuthenticationSecretFile == "" {
+// 		e = errors.New("Cannot have empty Marathon authentication file if principal is specified")
+// 	}
+// 	if c.MarathonAuthenticationPrincipal != "" && c.MarathonUser != "" {
+// 		e = errors.New("Cannot specify both MarathonUser and MarathonPrincipal")
+// 	}
+// 	if c.KVServer == "" {
+// 		e = errors.New(KVServerErr + c.KVServer)
+// 	}
+// 	rangeVals := strings.Split(c.ProxyPortRange, ":")
+// 	if len(rangeVals) != 2 {
+// 		e = errors.New(ProxyPortRangeFormatErr + c.ProxyPortRange)
+// 	} else {
+// 		if _, err := strconv.Atoi(rangeVals[0]); err != nil {
+// 			e = errors.New(ProxyPortRangeMinErr + c.ProxyPortRange)
+// 		}
+// 		if _, err := strconv.Atoi(rangeVals[1]); err != nil {
+// 			e = errors.New(ProxyPortRangeMaxErr + c.ProxyPortRange)
+// 		}
+// 	}
+// 	if !strings.Contains("json-file journald", c.AppLogDriver) {
+// 		e = errors.New(LogErr + c.AppLogDriver)
+// 	}
+// 	if c.AquaEnabled && c.AquaEndpoint == "" {
+// 		e = errors.New(AquaErr)
+// 	}
+// 	for _, param := range c.AllowedDockerRunParams {
+// 		if strings.ContainsAny(param, "[]\"") {
+// 			e = errors.New(
+// 				"FD_ALLOWED_DOCKER_RUN_PARAMS should be a comma-separated list of param names; no brackets or quotes. E.g.:\n" +
+// 					"    label,l,read-only,work-dir,w,network,net\n")
+// 		}
+// 	}
+// 	return e
 // }
 
 func setBoolValueFromEnv(field *bool, envVar string) {
@@ -172,7 +242,7 @@ func setBoolValueFromEnv(field *bool, envVar string) {
 		var err error
 		*field, err = strconv.ParseBool(env)
 		if err != nil {
-			log.Error("Invalid environment variable", "var", envVar, "value", env)
+			chatLog.Error("Invalid environment variable", "var", envVar, "value", env)
 		}
 	}
 }
--- main.go
+++ main.go
@@ -11,17 +11,22 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"runtime/debug"
+	"strings"
+	"syscall"
 
 	"flag"
 	"fmt"
 
 	// cli "github.com/behance/go-cli"
-	"github.com/behance/go-logging/formatters"
-	"github.com/behance/go-logging/log"
+
+	"github.com/bossjones/go-chatbot-lab/config"
+	logUtils "github.com/bossjones/go-chatbot-lab/shared/log-utils"
 	"github.com/bossjones/go-chatbot-lab/shared/version"
 )
 
-type ConfigValue string
+// type ConfigValue string
 
 var (
 	argConfigFile   string
@@ -38,49 +43,163 @@ func parseFlags() {
 	flag.Parse()
 }
 
-func setupLogging(logLevel string, isDebug bool, logLocation string, logAppName string) {
-	os.Setenv("LOG_APP_NAME", logAppName)
-	log.AlwaysShowColors(true)
-	if logLevel == "debug" || isDebug {
-		log.SetLevel(log.DebugLevel)
-	} else {
-		log.SetLevel(log.InfoLevel)
+// func isFile(path string) bool {
+// 	_, err := os.Stat(path)
+// 	return !os.IsNotExist(err)
+// }
+
+func panicGuard() {
+	panicMsg := recover()
+	if panicMsg != nil {
+		gLogger.Error(fmt.Sprintf("PANIC CAUGHT: %+v", panicMsg))
 	}
 
-	log.SetFormatter(formatters.SumologicFormatter{})
-
-	// set the log location, defaulting to stdout
-	if logLocation != "stdout" {
-		logFile, err := os.OpenFile(logLocation, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			log.Errorf("error opening log file: %v", err)
-		}
-		defer logFile.Close()
-		log.SetOutput(logFile)
+	stack := string(debug.Stack())
+	if !strings.Contains(os.Getenv("CHATBOT_CLUSTER_INFO"), "dev/local") {
+		stack = strings.Replace(stack, "\n", " -- ", -1)
 	}
+	gLogger.Error(fmt.Sprintf("BEGIN PANIC FULLSTACK: %s", stack))
+	gLogger.Error("Exiting...")
+
+	os.Exit(1)
 }
+
+func setupLogger() {
+	gLogger = logUtils.NewLogger()
+}
+
+func createConfig() config.Config {
+	if argConfigFile == "" {
+		gLogger.Info("No config file specified. Attempting to generate from ENV")
+	}
+	cfg, err := config.NewConfig(argConfigFile)
+	if err != nil {
+		gLogger.Crit("Error reading config.", "error", err)
+	}
+	// For local dev environments, we print the config as pretty JSON.
+	// Otherwise, we show it all on one line to be more Splunk-friendly.
+	if strings.Contains(os.Getenv("CHATBOT_CLUSTER_INFO"), "dev/local") {
+		gLogger.Info(fmt.Sprintf("Chatbot Config: %+v", cfg))
+	} else {
+		gLogger.Info("**** Chatbot Config ****", "config", cfg)
+	}
+	if cfg.Debug {
+		logUtils.SetLevel(logUtils.DebugLevel)
+	}
+
+	return cfg
+}
+
+// func setupLogging(logLevel string, isDebug bool, logLocation string, logAppName string) {
+// 	os.Setenv("LOG_APP_NAME", logAppName)
+// 	log.AlwaysShowColors(true)
+// 	if logLevel == "debug" || isDebug {
+// 		log.SetLevel(log.DebugLevel)
+// 	} else {
+// 		log.SetLevel(log.InfoLevel)
+// 	}
+
+// 	log.SetFormatter(formatters.SumologicFormatter{})
+
+// 	// set the log location, defaulting to stdout
+// 	if logLocation != "stdout" {
+// 		logFile, err := os.OpenFile(logLocation, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+// 		if err != nil {
+// 			log.Errorf("error opening log file: %v", err)
+// 		}
+// 		defer logFile.Close()
+// 		log.SetOutput(logFile)
+// 	}
+// }
 
 // Golang application auto build versioning
 // SOURCE: https://stackoverflow.com/questions/11354518/golang-application-auto-build-versioning
 
 func main() {
 
-	versionFlag := flag.Bool("version", false, "Version")
-	// flag provided but not defined: -ginkgo.seed
-	// SOURCE: https://github.com/onsi/ginkgo/issues/296#issuecomment-249924522
-	flag.Parse()
+	// set up signal handler
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt)
+	signal.Notify(sigchan, syscall.SIGTERM)
 
-	// If defined, show value
-	if *versionFlag {
-		fmt.Println("Git Commit:", version.GitCommit)
-		fmt.Println("Version:", version.Version)
-		if version.VersionPrerelease != "" {
-			fmt.Println("Version PreRelease:", version.VersionPrerelease)
-		}
-		// Return multiple named results simply by return
-		// versionFlag, flag, and fmt will be returned(?)
-		return
+	defer panicGuard()
+
+	parseFlags()
+
+	if flagShowVersion {
+		fmt.Println(version.Version)
+		os.Exit(0)
 	}
+	setupLogger()
 
-	fmt.Println("Hello.")
+	// var db db.Wrapper
+	cfg := createConfig()
+
+	gLogger.Info("Config created", cfg)
+
+	// TODO: Consider enabling this
+	// logCtx := log15.Ctx{
+	// 	"db_timeout_num_seconds": dbTimeoutNumSeconds,
+	// 	"db_engine":              cfg.DBEngine,
+	// 	"db_path":                cfg.DBPath,
+	// }
+	// gLogger.Info("Initializing database", logCtx)
+	// go initDB(cfg)
+	// select {
+	// case db = <-dbReady:
+	// 	gLogger.Info("Connected to database", logCtx)
+	// case <-time.After(dbTimeoutNumSeconds * time.Second):
+	// 	gLogger.Crit("Timeout waiting for database", logCtx)
+	// 	os.Exit(1)
+	// }
+
+	// kv := kvwrapper.NewKVWrapperWithAuth(
+	// 	[]string{cfg.KVServer},
+	// 	kvwrapper_etcd.EtcdWrapper{},
+	// 	cfg.KVUsername,
+	// 	cfg.KVPassword,
+	// )
+
+	// server, err := startServer(db, kv, cfg)
+	// if err != nil {
+	// 	gLogger.Error(fmt.Sprintf("Cannot start server because \"%s\"", err.Error()))
+	// 	os.Exit(2)
+	// }
+
+	<-sigchan
+	// server.Stop()
+	// for i := 0; i < numWorkers; i++ {
+	// 	// signal workers
+	// 	workerObjs[i].Stop()
+	// 	gLogger.Info(
+	// 		fmt.Sprintf("Worker %v stopped.", i),
+	// 		"worker_id", workerObjs[i].WorkerID,
+	// 	)
+	// }
+
+	// see ya
+	gLogger.Info("Goodbye")
+	os.Exit(0)
+
+	// TODO: Delete all of this
+	// ****************** OLD MAIN *******************************
+	// versionFlag := flag.Bool("version", false, "Version")
+	// // flag provided but not defined: -ginkgo.seed
+	// // SOURCE: https://github.com/onsi/ginkgo/issues/296#issuecomment-249924522
+	// flag.Parse()
+
+	// // If defined, show value
+	// if *versionFlag {
+	// 	fmt.Println("Git Commit:", version.GitCommit)
+	// 	fmt.Println("Version:", version.Version)
+	// 	if version.VersionPrerelease != "" {
+	// 		fmt.Println("Version PreRelease:", version.VersionPrerelease)
+	// 	}
+	// 	// Return multiple named results simply by return
+	// 	// versionFlag, flag, and fmt will be returned(?)
+	// 	return
+	// }
+
+	// fmt.Println("Hello.")
+	// ****************** OLD MAIN *******************************
 }
